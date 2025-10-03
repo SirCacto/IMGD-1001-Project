79df27f0c06f9d6b9b32ef98d997479849ad8cb6
Aluminator813 <aluminator813@gmail.com>
2025-10-03 10:52:49 -0400

    Fixed Slow Mo Glitch

--- Scripts/kill_zone.gd
+++ Scripts/kill_zone.gd
@@ -10,17 +10,13 @@ var player=player_path.instantiate()
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print ("You died!")
-		Engine.time_scale = .5
 		# animation_player.play("Death")
 		timer.start()
 	elif body.name =="SpacePlayer":
 		print ("You died!")
-		Engine.time_scale = .5
-		# animation_player.play("Death")
 		timer.start()
 
 
 
 func _on_timer_timeout() -> void:
-	Engine.time_scale = 1
 	get_tree().reload_current_scene()
