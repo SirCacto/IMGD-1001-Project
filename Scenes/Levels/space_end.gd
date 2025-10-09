a9753380f4797ad67e5a63711ff7d1bb9de30dc3
Aluminator813 <aluminator813@gmail.com>
2025-10-08 21:34:16 -0400

    Final Changes
    Fixed gun positions, background textures, end dialogue, added death dialogue, updated version notes.

--- Scenes/Levels/space_end.gd
+++ Scenes/Levels/space_end.gd
@@ -1,12 +1,21 @@
 extends Area2D
+@onready var space_player: CharacterBody2D = $SpacePlayer
 
+var end_not_reached = true
 
 func _on_body_entered(body: Node2D) -> void:
-	DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/space_success.dialogue"))
+	if (end_not_reached) :
+		DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/space_success.dialogue"))
+		end_not_reached = false
+
 	
 func _ready() -> void:
 	DialogueManager.dialogue_ended.connect(_on_dialogue_manager_dialogue_ended)
 
 func _on_dialogue_manager_dialogue_ended(resource: DialogueResource) -> void:
-	get_tree().change_scene_to_file("res://Scenes/Title Screen/titlescreen.tscn")
-	print ("End!")
+	if not (end_not_reached) :
+		get_tree().change_scene_to_file("res://Scenes/Title Screen/titlescreen.tscn")
+		print ("End!")
+	
+	if (end_not_reached) :
+		get_tree().reload_current_scene()
--- Scripts/kill_zone.gd
+++ Scripts/kill_zone.gd
@@ -4,19 +4,11 @@ extends Area2D
 var player_path=preload("res://Scenes/Player/player.tscn")
 var player=player_path.instantiate()
 
-@onready var timer: Timer = $Timer
-# @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print ("You died!")
-		# animation_player.play("Death")
-		timer.start()
+		DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/western_death.dialogue"))
 	elif body.name =="SpacePlayer":
 		print ("You died!")
-		timer.start()
-
-
-
-func _on_timer_timeout() -> void:
-	get_tree().reload_current_scene()
+		DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/space_death.dialogue"))
