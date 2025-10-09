294f6d17e7589b73285186e9fa5373886438c867
Aluminator813 <aluminator813@gmail.com>
2025-10-08 21:38:39 -0400

    Fixed Western Death

--- Scenes/Levels/western_end.gd
+++ Scenes/Levels/western_end.gd
@@ -2,6 +2,7 @@ extends Area2D
 @onready var black: Sprite2D = $"../../Black"
 @onready var player: CharacterBody2D = $"../../Player"
 
+var end_not_reached = true
 
 func _on_body_entered(body: Node2D) -> void:
 	# ADDING THIS WHEN I CAN FIGURE OUT HOW TO HAVE IT NOT GET STUCK ON THE DIALOGUE black.visible = !black.visible
@@ -12,5 +13,9 @@ func _ready() -> void:
 	DialogueManager.dialogue_ended.connect(_on_dialogue_manager_dialogue_ended)
 
 func _on_dialogue_manager_dialogue_ended(resource: DialogueResource) -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/space_level.tscn")
-	print ("To Space!")
+	if not (end_not_reached) :
+		get_tree().change_scene_to_file("res://Scenes/Levels/space_level.tscn")
+		print ("To Space!")
+
+	if (end_not_reached) :
+		get_tree().reload_current_scene()
